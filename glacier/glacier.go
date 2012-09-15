c358207038d9517d3f01df2d69cf220c2159d051
Robert Williamson <williamson.robert@gmail.com>
2012-09-14 21:29:47 -0500

    Reading and writing of a tree hash while I plan how to handle multipart uploads.

--- glacier/glacier.go
+++ glacier/glacier.go
@@ -1,7 +1,10 @@
 package glacier
 
 import (
+	"encoding/gob"
+	"fmt"
 	"github.com/rdwilliamson/aws"
+	"io"
 	"net/http"
 )
 
@@ -19,6 +22,44 @@ func NewConnection(secret, access string, r *aws.Region) *Connection {
 	// TODO a go routine to create a new signature when the date changes?
 	return &Connection{defaultClient, aws.NewSignature(secret, access,
 		r, "glacier")}
+}
+
+func CreateTreeHash(r io.Reader, w io.Writer) error {
+	th, err := createTreeHash(r)
+	if err != nil {
+		return err
+	}
+	printTreeHash(th, 0)
+
+	enc := gob.NewEncoder(w)
+	err = enc.Encode(th)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadTreeHash(r io.Reader) error {
+	dec := gob.NewDecoder(r)
+	var th treeHash
+	err := dec.Decode(&th)
+	if err != nil {
+		return err
+	}
+	printTreeHash(&th, 0)
+	return nil
+}
+
+func printTreeHash(th *treeHash, level int) {
+	if th.Left == nil && th.Right == nil {
+		fmt.Println("leaf", string(toHex(th.Hash[:])))
+	} else {
+		fmt.Println(level, string(toHex(th.Hash[:])))
+		level++
+		printTreeHash(th.Left, level)
+		printTreeHash(th.Right, level)
+	}
 }
 
 // TODO method to log things such as x-amzn-RequestId
