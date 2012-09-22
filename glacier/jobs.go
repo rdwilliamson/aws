1c5cf73fef8827be65a9984a5af91585b832e704
Robert Williamson <williamson.robert@gmail.com>
2012-09-21 21:45:49 -0500

    Added initiate inventory job.

--- glacier/jobs.go
+++ glacier/jobs.go
@@ -1,7 +1,11 @@
 package glacier
 
 import (
+	"bytes"
 	"encoding/json"
+	"github.com/rdwilliamson/aws"
+	"io/ioutil"
+	"net/http"
 )
 
 type job struct {
@@ -20,10 +24,46 @@ func (c *Connection) InitiateInventoryJob(vault, description,
 	topic string) error {
 	j := job{Type: "inventory-retrieval", Description: description,
 		SNSTopic: topic}
-	_, err := json.Marshal(j)
+	rawBody, err := json.Marshal(j)
 	if err != nil {
 		return err
 	}
+	body := bytes.NewReader(rawBody)
+
+	request, err := http.NewRequest("POST",
+		"https://"+c.Signature.Region.Glacier+"/-/vaults/"+vault+"/jobs", body)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("x-amz-glacier-version", "2012-06-01")
+
+	err = c.Signature.Sign(request, body, nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := c.Client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != 202 {
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return err
+		}
+		err = response.Body.Close()
+		if err != nil {
+			return err
+		}
+		var e aws.Error
+		err = json.Unmarshal(body, &e)
+		if err != nil {
+			return err
+		}
+		return e
+	}
+
 	return nil
 }
 
