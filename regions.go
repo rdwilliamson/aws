135292d0b050c704b84939390614934076c1b00d
Robert Williamson <williamson.robert@gmail.com>
2012-10-07 11:44:09 -0500

    Added remaining glacier regions.

--- regions.go
+++ regions.go
@@ -13,6 +13,22 @@ var (
 		"US East (Northern Virginia)",
 		"us-east-1",
 		"glacier.us-east-1.amazonaws.com"}
+	USWest1 = &Region{
+		"US West (Northern California)",
+		"us-west-1",
+		"glacier.us-west-1.amazonaws.com"}
+	USWest2 = &Region{
+		"US West (Oregon)",
+		"us-west-2",
+		"glacier.us-west-2.amazonaws.com"}
+	EU = &Region{
+		"EU (Ireland)",
+		"eu-west-1",
+		"glacier.eu-west-1.amazonaws.com"}
+	AsiaPacific = &Region{
+		"Asia Pacific (Tokyo)",
+		"ap-northeast-1",
+		"glacier.ap-northeast-1.amazonaws.com"}
 )
 
-var Regions = []*Region{USEast}
+var Regions = []*Region{USEast, USWest1, USWest2, EU, AsiaPacific}
